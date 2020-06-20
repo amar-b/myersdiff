d196c4cf620ad32387c46c8a3ad0478d84d8734e
Amar Bab <amarbab2@gmail.com>
2020-06-20 19:57:30 -0400

    printer

--- myersdiff.py
+++ myersdiff.py
@@ -1,5 +1,6 @@
 import copy 
 from enum import Enum
+from functools import reduce
 
 class EditType(Enum):
     EQL = 0
@@ -7,10 +8,9 @@ class EditType(Enum):
     DEL = 2
 
 class Myersdiffer:
-    def __init__(self, A, B, comparer=lambda x,y: x == y):
+    def __init__(self, A, B):
         self.A = list(A)
         self.B = list(B)
-        self.comparer = comparer
 
     def _shortest_path(self):
         lenA, lenB = len(self.A), len(self.B)
@@ -19,7 +19,6 @@ class Myersdiffer:
         T = []
         
         for D in range(max_edit+1):
-        #    print(V)
             T.append(copy.deepcopy(V))
             for k in range(-D, D+1, 2):
                 x = V[k+1] if (k == -D or (k != D and V[k-1]<V[k+1])) else V[k-1]+1
@@ -34,7 +33,7 @@ class Myersdiffer:
         return T
 
     def _line_diff(self, x, y, x_prev, y_prev):
-        print((x_prev,y_prev), "=>", (x, y))
+        #print((x_prev,y_prev), "=>", (x, y))
         if x == x_prev:
             return (-1, y_prev+1, EditType.INS)
         elif y == y_prev:
@@ -66,10 +65,31 @@ class Myersdiffer:
 
     def diff(self):
         return self._backtrack(self._shortest_path())
+    
+    def print_diff(self):
+        view = {
+            EditType.EQL.value: (" ", "\033[39m"),
+            EditType.INS.value: ("+", "\033[92m"),
+            EditType.DEL.value: ("-", "\033[91m"),
+        }
+        
+        result = ""
+        for (old, new, editType) in self.diff():
+            sign, color = view[editType.value]
 
+            if editType == EditType.DEL:
+                value = self.B[old-1]
+            else:
+                value = self.B[new-1]
+
+            line = "{}{} {}\n".format(color, sign, value)
+            result = line + result
+        print(result + "\033[39m")
+        
 listX = [i for i in "ABCABBA"]
 listY = [i for i in "CBABAC"]
-differ = Myersdiffer(listX, listY, lambda x,y: x==y)
-a = [i for i in differ.diff()]
-# for v in differ.diff():
-#     print(v)
+differ = Myersdiffer(listX, listY)
+
+for v in differ.diff():
+    print(v)
+differ.print_diff()
